89c84ceac02838935e0b302065812522d996c6e1
Vianney Bouault <vbouault@hotmail.fr>
2020-03-11 14:51:52 +0100

    nouvelle police+ merge header et accueil

--- img-deroulante.js
+++ img-deroulante.js
@@ -2,11 +2,11 @@ var imgs=[];
 
 imgs[0]='https://static.hitek.fr/img/actualite/2017/01/13/fb_the-wall-from-the-south.jpg';
 imgs[1]='https://www.businessinsider.fr/content/uploads/2019/04/s05e06_-_kings_landing-785x588.jpg';
-imgs[2]='https://lh3.googleusercontent.com/proxy/Q9fxLSkFkConLsZl_BencEnjveRtPoWqdjtbTGnoTAsb2O0-61duK51sGEXSeMbn3FB9AM11uzKNjet8pZXMdBWk_kMSNK5fnBJhsI5HO5Rts8aLS5VAwI9PfHB_VQPoq5IhuAyBdqcMlg';
+imgs[2]='https://i.pinimg.com/originals/4e/e7/fe/4ee7fe4a81fe5a9b77a0d3459886bd7d.jpg';
 
 var nbImgs=imgs.length;
 var position=0;
-var time=2000;
+var time=4000;
 
 function changeImg(){
     document.getElementById("imgSlide").src=imgs[position];
@@ -40,5 +40,14 @@ function slide (x){
   /*    var attenteTransition= setTimeout(function(){
         document.getElementById("imgSlide").className="";
         document.getElementById('imgSlide').src = imgs[position];},200);  */
-
 }
+
+function drop() {
+  document.getElementsByClassName('dropdown')[0].classList.toggle('down');
+  
+    setTimeout(function() {
+      document.getElementsByClassName('dropdown')[0].style.overflow = ('hidden')
+    }, 500)
+} 
+  
+
