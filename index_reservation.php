b996c3f7830cc8556266d8d3ecca6b31861f884d
raphael-morales <raphael.morales@hotmail.fr>
2020-03-27 17:22:09 +0100

    ajout des validations dans le formulaire de réservation

--- index_reservation.php
+++ index_reservation.php
@@ -1,57 +1,92 @@
- <?php include('_header.html'); ?>
+ <?php include('_header.html');
+ var_dump($_POST);
+ $errors = [];
+
+ if (!empty($_POST)){
+     if (empty($_POST['destination'])){
+         $errors['destination'] = "veuillez selectionner une destination";
+     }
+
+     if (empty($_POST['start'])){
+         $errors['start'] = "veuillez selectionner une date de départ";
+     }
+
+     if (empty($_POST['return'])){
+         $errors['return'] = "veuillez selectionner une date d'arrivée";
+     }
+
+     if (empty($_POST['voyageur'])){
+         $errors['voyageur'] = "veuillez selectionner le nombre de voyageurs";
+     }
+
+
+ }
+
+ ?>
 
   <h1 class="reservation">Formulaire de réservation</h1>
 
 <section class="formulaire">
-<form method="post" action="">
-<section class="destination">
-  <label for="destination">Où?</label>
-<?php
-$options = [
-        '<option value="">Choisissez votre destination</option>',
-        '<option value="port_real">Port Real</option>',
-        '<option value="port_real">Port Real</option>',
-        '<option value="winterfell">Winterfell</option>'
-];
-?>
-  <select class="select" name="destination" id="destination" onclick = "changePrix()">
+<form method="post" >
+    <section class="destination">
+      <label for="destination">Où?</label>
+        <?php if (isset($errors['destination'])){ ?>
+        <p><small class="erros"><?php echo $errors['destination'] ?></small></p>
+        <?php } ?>
     <?php
-    foreach ($options as $option) {
-        echo $option;
-    }
+    $options = [
+            '<option value="">Choisissez votre destination</option>',
+            '<option value="port_real">Port Real</option>',
+            '<option value="winterfell">Winterfell</option>'
+    ];
     ?>
+      <select class="select" name="destination" id="destination" onclick = "changePrix()">
+        <?php
+        foreach ($options as $option) {
+            echo $option;
+        }
+        ?>
 
 
-  </select>
-</section>
+      </select>
+    </section>
 
 
 
-<section class="date">
-<section>
-  <label for="start">Départ</label>
-  <input type="date" id="start" name="start" min="2020-06-01" max="2021-12-24">
-</section>
-<section>
-  <label for="return">Retour</label>
-  <input type="date" id="return" name="return" min="2020-06-07" max="2021-12-31">
-</section>
-</section>
+    <section class="date">
+    <section>
+      <label for="start">Départ</label>
+        <?php if (isset($errors['start'])){ ?>
+            <p><small class="erros"><?php echo $errors['start'] ?></small></p>
+        <?php } ?>
+      <input type="date" id="start" name="start" min="2020-06-01" max="2021-12-24">
+    </section>
+    <section>
+      <label for="return">Retour</label>
+        <?php if (isset($errors['return'])){ ?>
+            <p><small class="erros"><?php echo $errors['return'] ?></small></p>
+        <?php } ?>
+      <input type="date" id="return" name="return" min="2020-06-07" max="2021-12-31">
+    </section>
+    </section>
 
-<section class="voyageur">
-  <label for="voyageur">Nombre de voyageurs</label>
-  <input id="voyageur" type="number" placeholder="Nombre de personnes" min="1" max="15" required onclick = "changePrix()">
-</section>
+    <section class="voyageur">
+      <label for="voyageur">Nombre de voyageurs</label>
+        <?php if (isset($errors['voyageur'])){ ?>
+            <p><small class="erros"><?php echo $errors['voyageur'] ?></small></p>
+        <?php } ?>
+      <input id="voyageur" name="voyageur" type="number" placeholder="Nombre de personnes" min="1" max="15" required onclick = "changePrix()">
+    </section>
 
-<section>
-  <article id="po"><span id="spanPO">0</span> <img src="https://vignette.wikia.nocookie.net/paradisebay/images/1/10/Gold.png/revision/latest?cb=20170612200026&path-prefix=fr" alt="pièce d'or"></article>
-</section>
+    <section>
+      <article id="po"><span id="spanPO">0</span> <img src="https://vignette.wikia.nocookie.net/paradisebay/images/1/10/Gold.png/revision/latest?cb=20170612200026&path-prefix=fr" alt="pièce d'or"></article>
+    </section>
 
-<section class="button">
-  <button type="button" class="btn btn-secondary btn-lg btn-block">Réservez</button>
-</section>
+    <section class="button">
+      <button type="submit" class="btn btn-secondary btn-lg btn-block">Réservez</button>
+    </section>
 </form>
-</section>
+    </section>
 
 
 <?php include('_footer.html'); ?>
