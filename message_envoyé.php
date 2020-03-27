ae34ade0e9e00e53a22fae558473fb7d0d801081
raphael-morales <raphael.morales@hotmail.fr>
2020-03-27 16:27:47 +0100

    simple mise en page du message recu

--- message_envoyé.php
+++ message_envoyé.php
@@ -1,6 +1,6 @@
 <?php include('_header.html');
 ?>
-<div>
+<div class="recu">
     <article class="message_recu">
         Merci <?php echo $_GET['genre'] . ' ' . $_GET['Nom'] . ' ' . $_GET['Prenom'] ?> pour votre message. <br> Un conseiller vous répondra à votre adresse "<?php echo $_GET['Email'] ?>" dans les plus bref delai pour répondre à votre message : <br>
         <?php echo $_GET['Message'] ?>
